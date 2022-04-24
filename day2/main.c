e3d892a77f23f325b292a209195cdfea8578b207
Thomas Dion-Grondin <tomtom9461@gmail.com>
2022-04-24 01:35:35 -0400

    Add part2 of day 2

--- day2/main.c
+++ day2/main.c
@@ -78,10 +78,48 @@ void part1(void) {
         }
     }
 
-    printf("(part1) Results : %d (horizontal) * %d (depth) = %d", horizontal, depth, horizontal * depth);
+    printf("(part1) Results : %d (horizontal) * %d (depth) = %d\n", horizontal, depth, horizontal * depth);
 }
 
 void part2(void) {
+    FILE *input_file  = fopen("input.txt", "r");
+
+    int nb_lines = get_num_lines_in_file(input_file);
+    fseek(input_file, 0, SEEK_SET);
+
+    char line[256];
+    Move input[nb_lines];
+
+    for (int i = 0; i < nb_lines; i++) {
+        fgets(line, sizeof(line), input_file);
+        char *ptr;
+
+        input[i].name = get_type_from_char(strtok (line," ")[0]);
+        input[i].units = (int) strtol(strtok (NULL, " "), &ptr, 10);
+    }
+
+    fclose(input_file);
+
+    int horizontal = 0;
+    int depth = 0;
+    int aim = 0;
+    for (int i = 0; i < nb_lines; i++) {
+        Move cur_input = input[i];
+
+        switch (cur_input.name) {
+            case FORWARD:
+                horizontal += cur_input.units;
+                depth += (cur_input.units * aim);
+                break;
+            case DOWN:
+                aim += cur_input.units;
+                break;
+            default:
+                aim -= cur_input.units;
+        }
+    }
+
+    printf("(part2) Results : %d (horizontal) * %d (depth) = %d", horizontal, depth, horizontal * depth);
 
 }
 
